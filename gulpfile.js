59e0f3bf3109e95bbbc703a1eeeff694c6deaefe
victordones <victorhguimaraes.vh@gmail.com>
2023-12-08 08:20:23 -0300

    Criando o hero e impportando a fonte Avenir

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 const gulp = require ('gulp');
 const sass = require('gulp-sass')(require('sass'));
-
+const imagemin = require('gulp-imagemin');
 
 function styles() {
     return gulp.src('./src/styles/*.scss')  //recupera os arquivos, coloca os arquivos no gulp
@@ -8,7 +8,14 @@ function styles() {
     .pipe(gulp.dest('./dist/css'));
 }
 
-exports.default = styles
+function images() {
+    return gulp.src('./src/images/**/*')  //recupera os arquivos, coloca os arquivos no gulp
+    .pipe(imagemin())
+    .pipe(gulp.dest('./dist/images'));
+}
+
+
+exports.default = gulp.parallel(styles, images)
 exports.watch = function() {
-    gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
+    gulp.watch('./src/styles/*.scss', gulp.parallel(styles)) //usar npm run build watch
 }
